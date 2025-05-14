9640c164b0fcef4fc22d371d5af92b58211038b3
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-05-14 18:27:42 +0000

    Complete Assignment

---
+++ src/main/java/Bird.java
@@ -0,0 +1,16 @@
+public class Bird extends Animal{
+    public Bird(String name, int age, String type, int position){
+        super(name, age, type, position);
+    }
+
+    @Override
+    public String animalSound(){
+        return "Chirp";
+    }
+
+    @Override
+    public void move(){
+        this.position+=50;
+    }
+
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+public class Cat extends Animal{
+    public Cat(String name, int age, String type, int position){
+        super(name, age, type, position);
+    }
+
+    @Override
+    public String animalSound(){
+        return "Meow";
+    }
+
+    @Override
+    public void move(){
+        this.position+=5;
+    }
+
+
+}
